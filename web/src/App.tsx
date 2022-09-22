26ad41f1daa7e9c3c03034475c2fbde09d8f464f
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-09-22 20:32:24 -0300

    add images

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,35 @@
 import './styles/main.css';
+import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
   return (
-    <div className='w-8 h-8'>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt=""/>
+
+      <h1 className='text-6xl text-white font-black'>
+        Your <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> is here.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
